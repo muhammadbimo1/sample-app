7c1e3761029638799d80c4dcac53142160d6e839
reymond.rolas <reymond.rolas@danamas.co.id>
2023-09-27 11:05:13 +0700

    mark_completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,3 +13,12 @@ export const increment = () => {
   
 export const addTask = (task) => ({ type: 'ADD_TASK', payload: task });
 export const removeTask = (id) => ({ type: 'REMOVE_TASK', payload: id });
+
+export const MARK_TASK_AS_DONE = 'MARK_TASK_AS_DONE';
+
+export function markTaskAsDone(taskId) {
+  return {
+    type: MARK_TASK_AS_DONE,
+    payload: taskId,
+  };
+}
